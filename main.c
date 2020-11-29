bd8c2fcb2430f4783e6f0eb7bf37e961a0626ef1
frank400 <j.victorhuguenin2018@gmail.com>
2020-11-28 21:20:42 -0300

    fix: muda nome de elefante.c para main.c

---
+++ main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+
+#define size 23
+
+void get_pedido(char *pedido,int tamanho){
+
+	printf("Para girar a imagem no sentido horario, digite R1\n");
+	printf("Para girar a imagem no sentido anti-horario, digite R2\n");
+	printf("Para espelhar a imagem verticalmente, digite EV\n");
+	printf("Para espelhar a imagem horizontalmente, digite EH\n");
+
+	fgets(pedido,tamanho,stdin);
+}
+void get_strings(int size_matrix, char grade[][size_matrix]){
+    for (int x=0;x<size_matrix;x++)
+    {
+    	fgets(grade[x],23,stdin);
+	}
+}
+
+int main()
+{
+    char grade[size][size];
+
+    printf("Digite o desenho que deseja modificar:\n");
+	get_strings(size,grade);
+	
+	char pedido[15];
+	get_pedido(pedido,15);
+
+	if (strcmp(pedido,"R1"))
+	{
+		for (int x=22;x>=0;x--)
+		{
+			for (int y=0;y<strlen(grade[x]);y++)
+			{
+				printf("%c",grade[x][y]);
+			}
+			printf("\n");
+		}
+	}
+
+	// for (int x=0;x<23;x=x+1)
+    // {
+    // 	for (int y=0;y<strlen(grade[x]);y=y+1)
+    // 	{
+    // 		printf("%c",grade[x][y]);
+	// 	}
+	// 	printf("\n");
+	// }
+	// printf("Rotacao horaria\n");
+	
+	// printf("Rotacao anti-horaria\n");
+	// for (int x=23;x>0;x--)
+    // {
+    // 	for (int y=0;y<strlen(grade[x]);y++)
+    // 	{
+    // 		printf("%c",grade[y][x]);
+	// 	}
+	// 	printf("\n");
+	// }
+	// printf("Espelhamento vertical\n");
+	// for (int x=23;x>0;x=x-1)
+    // {
+    // 	for (int y=0;y<strlen(grade[x]);y=y+1)
+    // 	{
+    // 		printf("%c",grade[x][y]);
+	// 	}
+	// 	printf("\n");
+	// }
+	// printf("Espelhamento horizontal\n");
+	// for (int x=0;x<23;x=x+1)
+    // {
+    // 	for (int y=strlen(grade[x]);y>0;y=y-1)
+    // 	{
+    // 		printf("%c",grade[x][y]);
+	// 	}
+	// 	printf("\n");
+	// }
+	return 0;
+}
+
